b24445eae3bae1b72e09091a03fa37df6887ee6f
sara5491 <saraelinevans@gmail.com>
2022-01-29 20:17:34 +0000

    add folders to explorer

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ require('dotenv').config();
 
 app.use(express.urlencoded( { extended: true }));
 app.use(express.static('public'));
+app.use(expressLayouts);
 
+app.set('layout', './layouts/main');
+
+const routes = require('.server/routes/blogRoutes.js')
+app.use('/', routes);
+
+app.listen(port, ()==> console.log('Listening to port ${port}'));
